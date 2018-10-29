5f8aaea00022b1d6794fbf784d7754f9abcedbb9
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2018-10-29 11:38:03 +0100

    En terning der kan slå, og printer resultat

--- src/main/java/Terning.java
+++ src/main/java/Terning.java
@@ -1,2 +1,19 @@
 public class Terning {
+
+    public static void main(String[] args){
+        int sider = 6;
+
+        int vaerdi;
+
+        vaerdi = (int)(Math.random()*sider)+1;
+
+        System.out.println(vaerdi);
+
+    }
+
+
+
+
+
+
 }
