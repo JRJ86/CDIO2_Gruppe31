654593908b5ecdcd1ed7e2152c6b047ab04ebb70
maltebp <maltebp94@gmail.com>
2018-10-30 16:36:39 +0100

    TekstController: opdateret metoden fjernEtiket(). - Omdøbt til findPrintbarTekst. - Ændret måden hvorpå den finder dette er blevet ændret til noget mere læsbart. - Ændret indikatoren til printBarTekst fra citationstegn " til stjerne *.

--- src/main/java/Tekst/TekstController.java
+++ src/main/java/Tekst/TekstController.java
@@ -12,7 +12,7 @@ public class TekstController {
         File[] sprogFiler = getSprogFiler();
         String[] sprog = new String[sprogFiler.length];
         for( int i = 0; i<sprogFiler.length; i++){
-            sprog[i] = fjernEtiket(getLinjeFraFil(sprogFiler[i],0));
+            sprog[i] = findPrintbarTekst(getLinjeFraFil(sprogFiler[i],0));
         }
         return sprog;
     }
@@ -35,19 +35,46 @@ public class TekstController {
         return nuvaerendeTekstFil;
     }
 
-    static String fjernEtiket(String tekst){
-        for(int i=0; i<tekst.length(); i++ ){
-            if(tekst.charAt(i)=='"'){
-                for(int ii=i+1; ii<tekst.length(); ii++ ){
-                    if(tekst.charAt(ii)=='"'){
-                        tekst = tekst.substring(i+1,ii);
-                        break;
-                    }
-                }
-                break;
-            }
+
+    /**
+     *  Finder selve teksten på linjen, der skal printes - dvs. teksten mellem stjernerne.
+     * @param linje linje tekst hvori man ønsker at finde den printbare tekst
+     * @return Teksten der skal printes i spillet
+     */
+    static String findPrintbarTekst(String linje){
+        String printBarTekst = "";
+
+        int startIndex = 0;
+        int slutIndex = 0;
+
+        // Tjekker at linjen overhovedet indeholder printbar tekst.
+        if(linje.contains("*")){
+
+            // Gennemgår hver char (bogstav) i String'en 'linje'
+            for(int i=0; i<linje.length()-2; i++ ){
+                /*  Tjekker kun til og med det 3. sidste char,
+                    da der er brug for mindst 2 chars efter stjernen
+                    (en tekst/char + en stjerne mere mere)
+                 */
+
+                // Checker om char er første citationstegn
+                if(linje.charAt(i)=='*'){
+                    startIndex = i;
+                    break;
+                }}
+
+            if( startIndex != 0){
+
+                // Leder efter næste citationstegn, ved at gennemgår resten af bogstaverne i 'linje'
+                for(int i=startIndex+1; i<linje.length(); i++ ){
+                    if(linje.charAt(i)=='*'){
+                        slutIndex = i;
+                        break; }} }
         }
-        return tekst;
+        if(startIndex != 0 && slutIndex != 0){ printBarTekst = linje.substring(startIndex,slutIndex); }
+
+        return printBarTekst;
+
     }
 
 
@@ -70,7 +97,7 @@ public class TekstController {
     static String getTekst(int linjeNummer){
         String tekst = getLinjeFraFil(getTekstFil(), linjeNummer);
 
-        return fjernEtiket(tekst);
+        return findPrintbarTekst(tekst);
     }
 
     private static void getFile()throws IOException {
