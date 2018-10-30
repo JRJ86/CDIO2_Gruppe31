d1622ecc5b9001d5c9aa7fe749532b69a8259402
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2018-10-30 10:05:23 +0100

    Tror raflebæger er færdig (uden main tjekker metode) - ved godt at rul og set er den samme metode - Nu med comments

--- src/main/java/Raflebaeger.java
+++ src/main/java/Raflebaeger.java
@@ -1,28 +1,40 @@
+/**
+ * @Author Jacob
+ *Raflebaeger klassen, som har en Raflebaeger konstruktør
+ *og metoder der ruller med to terninger og returnerer samlr værdi.
+ */
+
 import java.util.Random;
 public class Raflebaeger {
 
+    //Variabel der beskriver terningesummen af kast med raflebæger
     private int terningSum;
 
+    // To Variabler der beskriver to terninger,
     private Terning terning1;
     private Terning terning2;
 
+    //Raflebaeger konstruktør, som indeholder to Terning objekter.
     public Raflebaeger()
     {
         terning1 = new Terning();
         terning2 = new Terning();
     }
 
+    //Metode der ruller med raflebægeret (skal måske revideres)
     public void rulRaflebaeger()
     {
         terningSum = terning1.rul()+terning2.rul();
 
     }
 
+    //Metode der returnerer den samlede terning værdi ved rul med raflebaeger
     public int getRaflebaeger()
     {
         return terningSum;
     }
 
+    //Samme metode som rulRaflebaeger (skal måske revideres)
     public void setRaflebaeger()
     {
         terningSum = terning1.rul()+terning2.rul();
--- src/main/java/Terning.java
+++ src/main/java/Terning.java
@@ -1,25 +1,35 @@
+/**
+ * @Author Jacob
+ * Terning klassen, som har en Terning konstruktør
+ * og metoder der ruller en terning og returner værdien.
+ */
+
 import java.util.Random;
 public class Terning
 {
-
+    //Variable der beskriver hvilken side der er op (sider) og max side side antal (MAX)
     private int sider;
     private final int MAX = 6;
 
+    //Konstruktør til at lave Terning objekter. Den sætter default værdi til 1, så man ikke kan slå 0
     public Terning()
     {
         sider = 1;
     }
 
+    //Ruller en terning og setter værdien til en integer fra 1-6 vha. Math.random (skal måske bruges)
     public void setSider()
     {
         sider = (int)(Math.random()*MAX)+1;
     }
 
+    //Returnerer værdien af den rullede terning (skal måske bruges)
     public int getSider()
     {
         return sider;
     }
 
+    //Ruller med terning og returnerer værdi (den der bliver brugt)
     public int rul()
     {
         sider = (int)(Math.random()*MAX)+1;
