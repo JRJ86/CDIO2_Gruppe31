0b6e39ab5049f6e28750c3d995c512d2dc990a90
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2018-10-31 18:33:06 +0100

    spilfunktioner - rafleSum variabel og Raflebaeger objekt lavet. Der er også lavet en rafleKast metode. Den er ikke testet endnu.

--- src/main/java/Spilfunktioner.java
+++ src/main/java/Spilfunktioner.java
@@ -1,2 +1,16 @@
 public class Spilfunktioner {
+
+    int raflesum;
+    Raflebaeger raflebaeger = new Raflebaeger();
+
+    /**
+     * @author Jacob og Andreas
+     * Metode som kaster med raflebægeret og return vaerdien
+     */
+    public int rafleKast(){
+
+        raflesum = raflebaeger.getTerningSum();
+        return raflesum;
+
+    }
 }
