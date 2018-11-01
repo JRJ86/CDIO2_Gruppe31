245f005f9c1615a8c463c6d7026d4616829a1e06
Andreas <s130016@student.dtu.dk>
2018-11-01 17:59:40 +0100

    Test_Spilfunktioner: Test af metoden, justerSpillerPenge. Testen godkendt

--- src/test/java/Test_Spilfunktioner.java
+++ src/test/java/Test_Spilfunktioner.java
@@ -15,4 +15,42 @@ public class Test_Spilfunktioner {
 
         }
     }
+
+    /**@author Andreas
+     * Tester om metoden justerSpillerPenge virker.
+     * Fremgangsmåde: Oprettet to objekter: begges pengebeholdning er som udgangspunkt = 1000. For spiller 1 bliver der trukket 500 fra beløbet og -
+     * for spiller 2 bliver der trukket 1002 fra det eksisterende beløb. Det kan forventes at beløbet for spiller 1 efter metoden er kladt er på 1500 og beløbet -
+     * for spiller 2 er 0 (ikke -2 idet der er lavet et if statement der lukker af for dette).
+     * Testen er godkendt.
+     */
+
+    @Test
+    public void Test_justerSpillerPenge() {
+        Spiller [] spillerListe= new Spiller[2];
+        spillerListe[0]=new Spiller();
+        spillerListe[0].setId(1);
+
+        spillerListe[1]=new Spiller();
+        spillerListe[1].setId(2);
+        int pengeJustering=-1002;
+        int pengeJusteringSP2=500;
+        Spiller a=new Spiller();
+        int spillerId=a.getId();
+
+
+        System.out.println("Spiller 1 har: "+spillerListe[0].getPenge());
+        System.out.println("Spiller 2 har "+spillerListe[1].getPenge());
+
+        int nyPenge=spillerListe[0].getPenge()+pengeJustering;
+        spillerListe[0].setPenge(nyPenge);
+
+        System.out.println("Efter pengejustering har spiller 1: "+ spillerListe[0].getPenge());
+
+        int nyPenge2=spillerListe[1].getPenge()+pengeJusteringSP2;
+        spillerListe[1].setPenge(nyPenge2);
+
+        System.out.println("Efter pengejustering har spiller 2: "+ spillerListe[1].getPenge());
+
+
+    }
 }
