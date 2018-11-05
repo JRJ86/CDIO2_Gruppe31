e882724fef37a4af909139a29e3818ea18c6fdc5
maltebp <maltebp94@gmail.com>
2018-11-05 12:24:07 +0100

    SpilController: Tilføjet tomme metoder til at opstille struktur

--- src/main/java/SpilController.java
+++ src/main/java/SpilController.java
@@ -1,2 +1,45 @@
 public class SpilController {
+
+    public static void main(String[] args) {
+        SpilController spilController = new SpilController();
+
+        spilController.koerSpil();
+    }
+
+    static void koerSpil(){
+
+        // Setup Spil
+
+        // Start Spil
+
+        // Kør tur
+
+        // For vinder
+
+        // Print vinder
+
+        // Slut Spil
+
+    }
+
+
+    static void spillerTur(Spiller spiller){
+
+        // Sla raflebæger (slaa og print)
+        // land på felt
+        // Juster spiller penge
+
+    }
+
+
+    static void setup(){
+        // Indstil Sprog
+
+        // Print spil intro
+
+        // Lav felter
+
+        // Lav spillere
+    }
+
 }
