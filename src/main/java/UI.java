ba00011da2ce45c216dc0639cf81ab4bd546e51e
FilipBLJensen <filblj@gmail.com>
2018-11-05 20:03:30 +0100

    UI: Filip: tilføjede startSpil metode
    tilføjede metode der starter spillet ved at indtaste 1

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,3 +1,5 @@
+import Tekst.Tekst;
+
 import Tekst.TekstController;
 
 import java.util.Scanner;
@@ -6,21 +8,52 @@ public class UI {
     Scanner input = new Scanner(System.in);
 
 
-    public void vaelgSprog(){
-        int sprog;
+    public static void main(String[] args) {
 
+        UI ui = new UI();
 
-        TekstController.getAlleSprog();
+        ui.startSpil();
 
-
-
-        TekstController.setSprog(input.nextInt());
 
     }
 
+
     public void printIntro(){}
 
-    public void startSpil(){}
+    /**
+     * @author Filip
+     * Metode der beder spilleren om at taste 1 for at starte spillet
+     * og fortsætter indtil spilleren angiver det rigtige input
+     */
+    public void startSpil(){
+
+        while (true){
+
+            System.out.println(Tekst.startSpil());
+
+            int tal = input.nextInt();
+
+            if (tal > 1){
+
+                System.out.println("Forkert input, prøv igen");
+
+            }
+
+            if (tal < 1){
+
+                System.out.println("Forkert input, prøv igen");
+
+            }
+
+            else if (tal == 1){
+
+                break;
+
+            }
+
+        }
+
+    }
 
     public void printTur(int spillerTur){}
 
@@ -36,14 +69,5 @@ public class UI {
 
     public void farvelPrint(){}
 
-
-    public static void main(String[] args) {
-        UI hej = new UI();
-
-        hej.vaelgSprog();
-
-
-
-    }
 
 }
