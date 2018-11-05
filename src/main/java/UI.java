880ebf8c0ac79ac52103159c2c9e3e5c53a6c4f8
Andreas <s130016@student.dtu.dk>
2018-11-05 18:59:36 +0100

    UI_Metoder,Andreas: Skabelon til UI-MEtoder er udarbejder

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,2 +1,24 @@
 public class UI {
+
+    public void vaelgSprog(){}
+
+    public void printIntro(){}
+
+    public void startSpil(){}
+
+    public void printTur(int spillerTur){}
+
+    public void slåRaflebaeger(){}
+
+    public void printTerning(int terningVerdi){}
+
+    public void printFelt(int FeltNr){}
+
+    public void printSpillerPenge(int spillerPenge){}
+
+    public void printVinder(int SpillerNr){}
+
+    public void farvelPrint(){}
+
+
 }
