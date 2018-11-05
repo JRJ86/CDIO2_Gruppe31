0ae6fa18eaeb9460ed4d1a1384325f5729eecf59
maltebp <maltebp94@gmail.com>
2018-11-05 18:49:37 +0100

    Tekst: Tilføjet menu tekster

--- src/main/java/Tekst/Tekst.java
+++ src/main/java/Tekst/Tekst.java
@@ -15,4 +15,19 @@ public class Tekst {
         return "";
     }
 
+    public static String spilNavn(){return TekstController.getTekst(23);}
+    public static String velkommen(){return TekstController.getTekst(24);}
+    public static String spilBeskrivelse(){return TekstController.getTekst(25);}
+    public static String saetSprog(){return TekstController.getTekst(27);}
+    public static String startSpil(){return TekstController.getTekst(28);}
+    public static String spillerTur1(){return TekstController.getTekst(29);}
+    public static String spillerTur2(){return TekstController.getTekst(30);}
+    public static String landetPaa(){return TekstController.getTekst(31);}
+    public static String penge1(){return TekstController.getTekst(32);}
+    public static String penge2(){return TekstController.getTekst(33);}
+    public static String farvel(){return TekstController.getTekst(34);}
+    public static String harVundet2(){return TekstController.getTekst(35);}
+    public static String spillerMedSmaat(){return TekstController.getTekst(36);}
+    public static String spillerMedStort(){return TekstController.getTekst(37);}
+
 }
