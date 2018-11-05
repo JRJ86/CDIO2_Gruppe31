19bd31bde59f592d76be2e899a37b858ef83143a
FilipBLJensen <filblj@gmail.com>
2018-11-05 18:57:01 +0100

    Tekst: Filip: tilføjet felttekster

--- src/main/java/Tekst/Tekst.java
+++ src/main/java/Tekst/Tekst.java
@@ -2,17 +2,84 @@ package Tekst;
 
 public class Tekst {
 
-
-    public static String guldMineTitel(){
-        return TekstController.getTekst(1);
+    public static String taarnetTitel() {
+        return TekstController.getTekst(3);
     }
 
-    public static String guldMineBeskrivelse(){
-        return TekstController.getTekst(2);
+    public static String taarnetBeskrivelse() {
+        return TekstController.getTekst(4);
+    }
+
+    public static String kraterTitel() {
+        return TekstController.getTekst(5);
+    }
+
+    public static String kraterBeskrivelse() {
+        return TekstController.getTekst(6);
+    }
+
+    public static String paladsetTitel() {
+        return TekstController.getTekst(7);
+    }
+
+    public static String paladsetBeskrivelse() {
+        return TekstController.getTekst(8);
+    }
+
+    public static String koldOerkenTitel() {
+        return TekstController.getTekst(9);
+    }
+
+    public static String koldOerkenBeskrivelse() {
+        return TekstController.getTekst(10);
+    }
+
+    public static String muretByTitel() {
+        return TekstController.getTekst(11);
+    }
+
+    public static String muretByBeskrivelse() {
+        return TekstController.getTekst(12);
     }
 
     public static String klosterTitel() {
-        return "";
+        return TekstController.getTekst(13);
+    }
+
+    public static String klosterBeskrivelse() {
+        return TekstController.getTekst(14);
+    }
+
+    public static String moerkGrotteTitel() {
+        return TekstController.getTekst(15);
+    }
+
+    public static String moerkGrotteBeskrivelse() {
+        return TekstController.getTekst(16);
+    }
+
+    public static String bjergHytteTitel() {
+        return TekstController.getTekst(17);
+    }
+
+    public static String bjergHytteBeskrivelse() {
+        return TekstController.getTekst(18);
+    }
+
+    public static String varulveMurenTitel() {
+        return TekstController.getTekst(19);
+    }
+
+    public static String varulveMurenBeskrivelse() {
+        return TekstController.getTekst(20);
+    }
+
+    public static String guldMineTitel() {
+        return TekstController.getTekst(21);
+    }
+
+    public static String guldMineBeskrivelse() {
+        return TekstController.getTekst(22);
     }
 
 }
