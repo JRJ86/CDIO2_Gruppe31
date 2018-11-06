556c107738226ffbe751bd998a06f967bd2eb8be
Jacob <Jacob_ziska@hotmail.com>
2018-11-06 21:07:06 +0100

    SpilController - færdiggjort spilController, så spillet fungerer for én enkelt spiller, dvs. vi mangler at gøre den i stand til at spille med flere spillere

--- src/main/java/SpilController.java
+++ src/main/java/SpilController.java
@@ -13,12 +13,16 @@ public class SpilController {
 
         ui.startSpil();
 
-        spillerTur(Spilfunktioner.getSpiller(1));
+        do {
+            spillerTur(Spilfunktioner.getSpiller(1));
+        }
+        while (!Spilfunktioner.harSpillerVundet(Spilfunktioner.getSpiller(1)));
 
-        Spilfunktioner.harSpillerVundet(Spilfunktioner.getSpiller(1));
 
+        ui.printVinder(1);
         // Print vinder
 
+        ui.farvelPrint();
         // Slut Spil
 
     }
@@ -26,13 +30,23 @@ public class SpilController {
 
     void spillerTur(Spiller spiller){
 
+        ui.printTur(spiller.getId());
+
+        ui.printSpillerPenge(Spilfunktioner.getSpillerPenge(spiller));
+
         ui.slåRaflebaeger();
 
         int feltnummer = Spilfunktioner.rafleKast();
 
+        ui.printTerning(feltnummer);
+
         int point = Spilfunktioner.getVaerdi(feltnummer-2); //feltnummer listen går fra 0 til 10, men terningeslagene går fra 2 til 12, derfor -2
 
+        ui.printFelt(feltnummer);
+
         Spilfunktioner.justerSpillerPenge(spiller , point);
+
+        ui.printSpillerPenge(Spilfunktioner.getSpillerPenge(spiller));
 
     }
 
